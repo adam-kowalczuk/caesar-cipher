6c60961896fdff26a6dd568bd92f3b3546082cc2
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-07-04 20:38:45 -0600

    Create 'decrypt' function and add 'direction' conditional statement

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -63,12 +63,27 @@ def encrypt(plain_text, shift_amount):
     cipher_text = ""
     # Loop through each letter of plain_text input
     for letter in plain_text:
-        # Get shifted index
+        # Get shifted index of each letter
         index = alphabet.index(letter) + shift_amount
-        # Add letter at shifted index to cipher text
+        # Add letter at shifted index to cipher_text
         cipher_text += alphabet[index]
 
     print(f"The encoded text is {cipher_text}")
 
 
-encrypt(plain_text=text, shift_amount=shift)
+def decrypt(cipher_text, shift_amount):
+    plain_text = ""
+    # Loop through each letter of the cipher_text input
+    for letter in cipher_text:
+        # Get shifted index of each letter
+        index = alphabet.index(letter) - shift_amount
+        # Add letter at shifted index to plain_text
+        plain_text += alphabet[index]
+    print(f"The decoded text is {plain_text}.")
+
+
+# Check whether user wants to 'encode' or 'decode' message, then call appropriate function
+if direction == "encode":
+    encrypt(plain_text=text, shift_amount=shift)
+else:
+    decrypt(cipher_text=text, shift_amount=shift)
