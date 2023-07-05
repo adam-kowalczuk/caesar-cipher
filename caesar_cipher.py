53126bbc3d81f50ea54780545e8afc7105661b27
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-07-04 18:56:27 -0600

    Duplicate 'alphabet' letters to accomodate shifted letters that are greater than alphabet length

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -25,6 +25,32 @@ alphabet = [
     "x",
     "y",
     "z",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
 ]
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
@@ -44,9 +70,5 @@ def encrypt(plain_text, shift_amount):
 
     print(f"The encoded text is {cipher_text}")
 
-
-##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
-
-# TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
 
 encrypt(plain_text=text, shift_amount=shift)
